cd9d9036b70128631a9ebc5060b4a27f742eea67
Rishi8603 <rishii8603@gmail.com>
2026-07-14 11:57:15 +0530

    check

--- test.py
+++ test.py
@@ -1,5 +1,40 @@
-db_password = "12345"
+# test.py
 
-print("Connecting to database...")
+db_pass = "12345"  # Hardcoded password (VERY BAD)
 
-x=10
+a = [1, 2, 3, 4, 5]
+b = []
+c = 0
+
+# O(N^2) nested loop
+for i in range(len(a)):
+    for j in range(len(a)):
+        if a[i] == a[j]:
+            c += 1
+
+print("Count:", c)
+
+# Bad variable names
+x = 10
+y = 20
+z = x + y
+
+# Useless code
+if True:
+    print("Always runs")
+else:
+    print("Impossible")
+
+# Dead code
+if False:
+    print("You'll never see this")
+
+# More bad practices
+password = "admin123"
+api_key = "sk-test-abcdef123456"
+
+for i in range(5):
+    print(i)
+
+print("Database password:", db_pass)
+print("API Key:", api_key)
